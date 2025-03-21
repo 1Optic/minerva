28d28672d72d14f57e57307ccb5f166a0bfaea81
Alfred Blokland <alfred.blokland@1optic.io>
2025-03-21 15:11:43 +0100

    Remove focus feature

--- crates/cli/commands/graph.rs
+++ crates/cli/commands/graph.rs
@@ -4,11 +4,6 @@ use async_trait::async_trait;
 use clap::Parser;
 
 use minerva::instance::MinervaInstance;
-use petgraph::data::ElementIterator;
-use petgraph::{
-    prelude::GraphMap,
-    visit::{DfsPostOrder, Walker},
-};
 
 use super::common::{connect_to_db, get_db_config, Cmd, CmdResult};
 
@@ -16,8 +11,6 @@ use super::common::{connect_to_db, get_db_config, Cmd, CmdResult};
 pub struct GraphOpt {
     #[arg(long = "from-dir", help = "load Minerva instance from directory")]
     from_dir: Option<PathBuf>,
-    #[arg(long, help = "focus on specific node")]
-    focus_on: Option<String>,
     #[arg(long)]
     dependency_order: bool,
 }
@@ -36,34 +29,11 @@ impl Cmd for GraphOpt {
             }
         };
 
-        match &self.focus_on {
-            None => {
-                let graph = instance.dependency_graph();
-                let dot =
-                    petgraph::dot::Dot::with_config(&graph, &[petgraph::dot::Config::EdgeNoLabel]);
+        let graph = instance.dependency_graph();
+        let dot =
+            petgraph::dot::Dot::with_config(&graph, &[petgraph::dot::Config::EdgeNoLabel]);
 
-                println!("{}", dot);
-            }
-            Some(node_ref) => {
-                let graph = instance.dependency_graph();
-
-                let index = graph
-                    .node_indices()
-                    .find(|i| graph[*i].to_string().eq(node_ref))
-                    .unwrap();
-
-                let mut dfs = DfsPostOrder::new(&graph, index);
-
-                let graph_map = GraphMap::from_iter(dfs.iter(&graph));
-
-                let dot = petgraph::dot::Dot::with_config(
-                    &graph_map,
-                    &[petgraph::dot::Config::EdgeNoLabel],
-                );
-
-                println!("{}", dot);
-            }
-        };
+        println!("{}", dot);
 
         Ok(())
     }
