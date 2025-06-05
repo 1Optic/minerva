5fe64746a818206184b86f76ace9db61484bd142
Alfred Blokland <alfred.blokland@1optic.io>
2025-06-05 23:55:53 +0200

    WIP

--- crates/cli/commands/start.rs
+++ crates/cli/commands/start.rs
@@ -2,17 +2,21 @@ use std::env;
 use std::fs::File;
 use std::io::{BufWriter, Write};
 use std::path::PathBuf;
+use std::borrow::Cow;
 
 use log::info;
 
 use async_trait::async_trait;
 use clap::Parser;
+use ratatui::widgets::{Block, Borders};
 use serde::{Deserialize, Serialize};
 
 use tokio::signal;
 use ratatui::Frame;
-use ratatui::{prelude::*, widgets::*};
+use ratatui::prelude::*;
 use futures::{future::FutureExt, StreamExt};
+use tui_logger::*;
+use unicode_segmentation::UnicodeSegmentation;
 
 use minerva::cluster::{BuildImageProvider, MinervaCluster, MinervaClusterConfig};
 use minerva::error::{Error, RuntimeError};
@@ -46,19 +50,22 @@ struct ClusterConfig {
 
 fn render(frame: &mut Frame) {
     let [output_area, log_area] = Layout::vertical([
-	Constraint::Fill(50),
-	Constraint::Fill(50),
+        Constraint::Fill(50),
+        Constraint::Fill(50),
     ])
     .areas(frame.area());
 
-
     frame.render_widget("Hello world", output_area);
 
-    let filter_state = tui_logger::TuiWidgetState::new()
-        .set_default_display_level(tui_logger::LevelFilter::Off)
-        .set_level_for_target("db_err", tui_logger::LevelFilter::Off);
+    let formatter = Box::new(CustomFormatter::default());
 
-    let logger_widget = tui_logger::TuiLoggerWidget::default().state(&filter_state);
+    let logger_widget = tui_logger::TuiLoggerWidget::default()
+        .block(
+            Block::default()
+                .title("logs")
+                .borders(Borders::ALL)
+        )
+        .opt_formatter(Some(formatter));
 
     frame.render_widget(logger_widget, log_area);
 }
@@ -68,8 +75,15 @@ async fn gui() {
     let mut terminal = ratatui::init();
 
     loop {
-        let mut delay = tokio::time::sleep(tokio::time::Duration::from_millis(100));
-        terminal.draw(render).unwrap();
+        let delay = tokio::time::sleep(tokio::time::Duration::from_millis(100));
+        let draw_result = terminal.draw(render);
+
+        match draw_result {
+            Ok(_) => {},
+            Err(e) => {
+                println!("error in GUI: {e}");
+            }
+        }
 
         let event = reader.next().fuse();
 
@@ -78,11 +92,19 @@ async fn gui() {
             maybe_event = event => {
 
                 match maybe_event {
-                    Some(Ok(crossterm::event::Event::Key(_))) => {
-                        break;
+                    Some(Ok(crossterm::event::Event::Key(k))) => {
+                        match k.code {
+                            crossterm::event::KeyCode::Esc => {
+                                break;
+                            },
+                            crossterm::event::KeyCode::Char('q') => {
+                                break;
+                            },
+                            _ => {}
+                        }
                     },
                     Some(Err(_)) => todo!(),
-       	            None => {
+                    None => {
                         //break;
                     },
                     _ => todo!(),
@@ -97,15 +119,10 @@ async fn gui() {
 #[async_trait]
 impl Cmd for StartOpt {
     async fn run(&self) -> CmdResult {
-        let drain = tui_logger::Drain::new();
-	env_logger::Builder::default()
-            .format(move |buf, record|
-                Ok(drain.log(record))
-            ).init();
-        //tui_logger::init_logger(tui_logger::LevelFilter::Trace).unwrap();
-        //tui_logger::set_default_level(tui_logger::LevelFilter::Trace);
+        tui_logger::init_logger(tui_logger::LevelFilter::Info).unwrap();
+        tui_logger::set_default_level(tui_logger::LevelFilter::Info);
 
-        tokio::spawn(gui());
+        let gui_task_handle = tokio::spawn(gui());
 
         let minerva_instance_root_option: Option<PathBuf> = match &self.instance_root {
             Some(root) => Some(root.clone()),
@@ -223,9 +240,15 @@ impl Cmd for StartOpt {
             cluster.controller_port, test_database.name
         );
 
-        signal::ctrl_c().await.map_err(|e| {
-            Error::Runtime(format!("Could not start waiting for Ctrl-C: {e}").into())
-        })?;
+        tokio::select! {
+            _ = gui_task_handle => {
+            }
+            _ = signal::ctrl_c() => {}
+        }
+
+        //signal::ctrl_c().await.map_err(|e| {
+        //    Error::Runtime(format!("Could not start waiting for Ctrl-C: {e}").into())
+        //})?;
 
         Ok(())
     }
@@ -240,5 +263,129 @@ fn write_env_file(file_path: &str, env: &[(String, String)]) {
         env_buf_writer
             .write_fmt(format_args!("{name}={value}\n"))
             .unwrap();
+    }
+}
+
+#[derive(Default)]
+pub struct CustomFormatter {
+    /// Base style of the widget
+    pub style: Style,
+    /// Level based style
+    pub style_error: Option<Style>,
+    pub style_warn: Option<Style>,
+    pub style_debug: Option<Style>,
+    pub style_trace: Option<Style>,
+    pub style_info: Option<Style>,
+    pub format_separator: char,
+    pub format_timestamp: Option<String>,
+    pub format_output_level: Option<TuiLoggerLevelOutput>,
+    pub format_output_target: bool,
+    pub format_output_file: bool,
+    pub format_output_line: bool,
+}
+
+impl CustomFormatter {
+    fn append_wrapped_line(
+        &self,
+        style: Style,
+        indent: usize,
+        lines: &mut Vec<Line>,
+        line: &str,
+        width: usize,
+        with_indent: bool,
+    ) {
+        let mut p = 0;
+        let mut wrap_len = width;
+        if with_indent {
+            wrap_len -= indent;
+        }
+        let space = " ".repeat(indent);
+        let line_chars = line.graphemes(true).collect::<Vec<_>>();
+        while p < line_chars.len() {
+            let linelen = std::cmp::min(wrap_len, line_chars.len() - p);
+            let subline = &line_chars[p..p + linelen];
+
+            let mut spans: Vec<Span> = Vec::new();
+            if wrap_len < width {
+                // need indent
+                spans.push(Span {
+                    style,
+                    content: Cow::Owned(space.to_string()),
+                });
+            }
+            spans.push(Span {
+                style,
+                content: Cow::Owned(subline.iter().map(|x| x.to_string()).collect()),
+            });
+            let line = Line::from(spans);
+            lines.push(line);
+
+            p += linelen;
+            // following lines need to be indented
+            wrap_len = width - indent;
+        }
+    }
+}
+
+impl LogFormatter for CustomFormatter {
+    fn min_width(&self) -> u16 {
+        9 + 4
+    }
+    fn format(&self, width: usize, evt: &ExtLogRecord) -> Vec<Line> {
+        let mut lines = Vec::new();
+        let mut output = String::new();
+        let (col_style, lev_long, lev_abbr, with_loc) = match evt.level {
+            log::Level::Error => (self.style_error, "ERROR", "E", true),
+            log::Level::Warn => (self.style_warn, "WARN ", "W", true),
+            log::Level::Info => (self.style_info, "INFO ", "I", true),
+            log::Level::Debug => (self.style_debug, "DEBUG", "D", true),
+            log::Level::Trace => (self.style_trace, "TRACE", "T", true),
+        };
+        let col_style = col_style.unwrap_or(self.style);
+        if let Some(fmt) = self.format_timestamp.as_ref() {
+            output.push_str(&format!("{}", evt.timestamp.format(fmt)));
+            output.push(self.format_separator);
+        }
+        match &self.format_output_level {
+            None => {}
+            Some(TuiLoggerLevelOutput::Abbreviated) => {
+                output.push_str(lev_abbr);
+                output.push(self.format_separator);
+            }
+            Some(TuiLoggerLevelOutput::Long) => {
+                output.push_str(lev_long);
+                output.push(self.format_separator);
+            }
+        }
+        if self.format_output_target {
+            output.push_str(&evt.target());
+            output.push(self.format_separator);
+        }
+        if with_loc {
+            if self.format_output_file {
+                if let Some(file) = evt.file() {
+                    output.push_str(file);
+                    output.push(self.format_separator);
+                }
+            }
+            if self.format_output_line {
+                if let Some(line) = evt.line {
+                    output.push_str(&format!("{}", line));
+                    output.push(self.format_separator);
+                }
+            }
+        }
+        let mut sublines: Vec<&str> = evt.msg().lines().rev().collect();
+
+        if !sublines.is_empty() {
+            output.push_str(sublines.pop().unwrap());
+            self.append_wrapped_line(col_style, 9, &mut lines, &output, width, false);
+
+            for subline in sublines.iter().rev() {
+                self.append_wrapped_line(col_style, 9, &mut lines, subline, width, true);
+            }
+        }
+
+        lines
     }
 }
