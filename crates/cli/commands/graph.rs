c72f95e09435640174a4c4156d8faf3ba8d7930a
Alfred Blokland <alfred.blokland@1optic.io>
2025-06-12 13:59:49 +0200

    Handle case of unmatched object name

--- crates/cli/commands/graph.rs
+++ crates/cli/commands/graph.rs
@@ -5,6 +5,7 @@ use clap::Parser;
 
 use super::common::{connect_to_db, get_db_config, Cmd, CmdResult};
 use minerva::{
+    error::RuntimeError,
     graph::{dependee_graph, dependency_graph, node_index_by_name, render_graph},
     instance::MinervaInstance,
 };
@@ -38,10 +39,14 @@ impl Cmd for GraphOpt {
         let full_graph = instance.dependency_graph();
 
         let graph = if let Some(start) = &self.dependencies {
-            let start_index = node_index_by_name(&full_graph, start).unwrap();
+            let start_index = node_index_by_name(&full_graph, start).ok_or(
+                RuntimeError::from_msg(format!("Could not find object named '{start}'")),
+            )?;
             dependency_graph(&full_graph, start_index)
         } else if let Some(start) = &self.dependees {
-            let start_index = node_index_by_name(&full_graph, start).unwrap();
+            let start_index = node_index_by_name(&full_graph, start).ok_or(
+                RuntimeError::from_msg(format!("Could not find object named '{start}'")),
+            )?;
             dependee_graph(&full_graph, start_index)
         } else {
             full_graph
