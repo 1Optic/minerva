95d284d95209e6542afcdfea08a2577464e7d6e3
André Engels <andre.engels@1optic.io>
2025-07-31 14:01:59 +0200

    Timestamptz2

---
+++ crates/minerva/migrations/V52__define_timestamptz_abbreviated_in_data_type_order.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION "trend_directory"."data_type_order"("data_type" text)
+    RETURNS integer
+AS $$
+BEGIN
+    CASE data_type
+        WHEN 'smallint' THEN
+            RETURN 1;
+        WHEN 'integer' THEN
+            RETURN 2;
+        WHEN 'bigint' THEN
+            RETURN 3;
+        WHEN 'real' THEN
+            RETURN 4;
+        WHEN 'double precision' THEN
+            RETURN 5;
+        WHEN 'numeric' THEN
+            RETURN 6;
+        WHEN 'timestamp without time zone' THEN
+            RETURN 7;
+        WHEN 'timestamp with time zone' THEN
+            RETURN 7;
+        WHEN 'timestamp' THEN
+            RETURN 7;
+        WHEN 'timestamptz' THEN
+            RETURN 7;
+        WHEN 'smallint[]' THEN
+            RETURN 8;
+        WHEN 'integer[]' THEN
+            RETURN 9;
+        WHEN 'numeric[]' THEN
+            RETURN 10;
+        WHEN 'text[]' THEN
+            RETURN 11;
+        WHEN 'text' THEN
+            RETURN 12;
+        WHEN NULL THEN
+            RETURN NULL;
+        ELSE
+            RAISE EXCEPTION 'Unsupported data type: %', data_type;
+    END CASE;
+END;
+$$ LANGUAGE plpgsql IMMUTABLE STRICT;
