a42797d2c52656c1968205ea3de5d954f1e76ab2
Alfred Blokland <alfred.blokland@1optic.io>
2026-06-25 15:28:24 +0200

    Add references to old definitions

--- crates/minerva/migrations/V62__attribute_id_bigint.sql
+++ crates/minerva/migrations/V62__attribute_id_bigint.sql
@@ -1,6 +1,7 @@
+-- 2,+25 replaces V10__main.sql 5528,+25
 CREATE OR REPLACE FUNCTION attribute_directory.create_at_func_ptr_sql(attribute_directory.attribute_store) RETURNS text[]
     LANGUAGE sql STABLE
-    AS $_$
+    AS $function$
 SELECT ARRAY[
         format(
             'CREATE FUNCTION attribute_history.%I(timestamp with time zone)
@@ -22,9 +23,9 @@ $$ LANGUAGE plpgsql STABLE',
             attribute_directory.at_ptr_function_name($1)
         )
     ];
-$_$;
+$function$;
 
-
+-- 29,+18 replaces V10__main.sql 4483,+18
 CREATE OR REPLACE FUNCTION attribute_directory.create_curr_ptr_table_sql(attribute_directory.attribute_store) RETURNS text[]
     LANGUAGE sql STABLE
     AS $_$
@@ -45,28 +46,7 @@ SELECT ARRAY[
 ];
 $_$;
 
-
-CREATE OR REPLACE FUNCTION attribute_directory.create_curr_ptr_table_sql(attribute_directory.attribute_store) RETURNS text[]
-    LANGUAGE sql STABLE
-    AS $_$
-SELECT ARRAY[
-    format('CREATE TABLE attribute_history.%I (
-        id bigint,
-        PRIMARY KEY (id))',
-        attribute_directory.curr_ptr_table_name($1)
-    ),
-    format(
-        'CREATE INDEX ON attribute_history.%I (id)',
-        attribute_directory.curr_ptr_table_name($1)
-    ),
-    format(
-        'ALTER TABLE attribute_history.%I OWNER TO minerva_writer',
-        attribute_directory.curr_ptr_table_name($1)
-    )
-];
-$_$;
-
-
+-- 50,+24 replaces V10__main.sql 5587,+24
 CREATE OR REPLACE FUNCTION attribute_directory.create_entity_at_func_ptr_sql(attribute_directory.attribute_store) RETURNS text[]
     AS $_$
 SELECT ARRAY[
@@ -93,39 +73,7 @@ $$ LANGUAGE plpgsql STABLE',
 ];
 $_$ LANGUAGE sql STABLE;
 
-
-CREATE OR REPLACE FUNCTION attribute_directory.create_entity_at_func_sql(attribute_directory.attribute_store) RETURNS text[]
-    AS $_$
-SELECT ARRAY[
-        format(
-            'CREATE FUNCTION attribute_history.%I(entity_id integer, timestamp with time zone)
-    RETURNS attribute_history.%I
-AS $$
-DECLARE
-  result attribute_history.%I;
-BEGIN
-  SELECT *
-    FROM attribute_history.%I
-    WHERE id = attribute_history.%I($1, $2)
-  INTO result;
-  RETURN result;
-END;
-$$ LANGUAGE plpgsql STABLE;',
-            attribute_directory.at_function_name($1),
-            attribute_directory.to_table_name($1),
-            attribute_directory.to_table_name($1),
-            attribute_directory.to_table_name($1),
-            attribute_directory.at_ptr_function_name($1)
-        ),
-        format(
-            'ALTER FUNCTION attribute_history.%I(entity_id integer, timestamp with time zone) '
-            'OWNER TO minerva_writer',
-            attribute_directory.at_function_name($1)
-        )
-    ];
-$_$ LANGUAGE sql STABLE;
-
-
+-- 77,+38 replaces V10__main.sql 4674,+38
 CREATE OR REPLACE FUNCTION attribute_directory.create_history_table_sql(attribute_directory.attribute_store) RETURNS text[]
     LANGUAGE sql STABLE
     AS $_$
@@ -166,7 +114,7 @@ SELECT ARRAY[
 ];
 $_$;
 
-
+-- 118,+13 replaces V10__main.sql 4935,+10
 DROP FUNCTION IF EXISTS attribute_directory.last_history_id(integer);
 
 CREATE FUNCTION attribute_directory.last_history_id(attribute_store_id integer) RETURNS bigint
@@ -175,7 +123,7 @@ DECLARE
   result bigint;
 BEGIN
   EXECUTE FORMAT(
-    'SELECT COALESCE(MAX(id), 0) FROM attribute_history.%I', 
+    'SELECT COALESCE(MAX(id), 0) FROM attribute_history.%I',
     attribute_directory.to_table_name(attribute_directory.get_attribute_store($1))
   ) INTO result;
   RETURN result;
