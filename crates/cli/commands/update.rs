ec1847378c25d1e38bf1a6e6a5de807f4582b18b
Chris <chris.janssen@1optic.io>
2026-07-06 15:23:02 +0200

    Restore setting environment variable

--- crates/cli/commands/update.rs
+++ crates/cli/commands/update.rs
@@ -85,7 +85,19 @@ impl UpdateOpt {
                 let instance_db = MinervaInstance::load_from_db(&mut client).await?;
 
                 let minerva_instance_root = match &self.instance_root {
-                    Some(root) => root.clone(),
+                    Some(root) => {
+                        // Next to passing on the Minerva instance root directory, we need to set the
+                        // environment variable for any child processes that might be started during
+                        // initialization.
+                        // SAFETY: This CLI updates process environment before spawning any child
+                        // process that depends on this variable, and this assignment is scoped to
+                        // command execution.
+                        unsafe {
+                            std::env::set_var(ENV_MINERVA_INSTANCE_ROOT, root);
+                        }
+
+                        root.clone()
+                    }
                     None => match env::var(ENV_MINERVA_INSTANCE_ROOT) {
                         Ok(v) => PathBuf::from(v),
                         Err(e) => {
