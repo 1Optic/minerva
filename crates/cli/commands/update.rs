631213670320388c1904d601351c1e5a88ae76d0
André Engels <andre.engels@1optic.io>
2026-08-10 15:18:48 +0200

    fix(db): Change ownership of objects to postgres

--- crates/cli/commands/update.rs
+++ crates/cli/commands/update.rs
@@ -285,6 +285,13 @@ async fn update(
         }
     }
 
+    client
+        .execute(
+            "SELECT directory.change_ownership_for_all_schemas($1)",
+            &[&"postgres"],
+        )
+        .await?;
+
     Ok(())
 }
 
